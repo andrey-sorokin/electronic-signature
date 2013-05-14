0536735693684ec180ac7513d7c09c8c9c716179
Andrey.Sorokin <Andrey.Sorokin@R-Style.com>
2013-05-14 17:08:16 +0400

    added new tests

--- sql2xml/src/test/java/ru/rstyle/so/main/MainTest.java
+++ sql2xml/src/test/java/ru/rstyle/so/main/MainTest.java
@@ -4,15 +4,47 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest {
-	private static String sqlQuery = "SELECT id, " +
-	                                 "XMLELEMENT(name document, " +
-	                                 "XMLATTRIBUTES(caption, description), " +
-	                                 "XMLELEMENT(name mnemo, mnemo)) " +
-	                                 "FROM sys_classes " +
-	                                 "WHERE id = (SELECT id FROM sys_classes  WHERE dataset = 'r2_doc') ";
-
-	@Test(enabled = false)
+	
+	@Test(enabled = true)
 	public void testMain() throws Exception {
-		Assert.assertTrue(Main.main(sqlQuery));
+		
+		String  [] sqlQuery  = new String [4];
+		
+		// test upper case + XMLFOREST function
+		sqlQuery[0] = "SELECT id, " +
+                "XMLELEMENT(name document, " +
+                "XMLFOREST(s.caption as \"Caption\", " +
+                "s.description as \"Description\")) " + 
+                "FROM sys_classes s " +
+                "WHERE id = (SELECT id FROM sys_classes WHERE dataset = 'r2_doc')";
+		
+        // test lower case + XMLATTRIBUTES element
+		sqlQuery[1] = "select id, " +
+                "xmlelement(name document, " +
+                "xmlattributes(caption, description), " +
+                "xmlelement(name mnemo, mnemo)) " +
+                "from sys_classes " +
+                "where id = (select id from sys_classes  where dataset = 'r2_doc') ";
+		
+		// test XMLCONCAT function
+		sqlQuery[2] =  "SELECT id, " +
+		        " xmlelement(name root, " + 
+                        "XMLCONCAT(XMLELEMENT(NAME \"caption\", s.caption), " +
+                                  "XMLELEMENT(NAME \"description\", s.description))) " +
+               "FROM sys_classes s " +
+               "WHERE id = (SELECT id FROM sys_classes WHERE dataset = 'r2_doc') ";
+		
+		// test XMLAGG function
+		sqlQuery[3] = "SELECT 1, " +
+				"XMLELEMENT(NAME \"root\", " +
+                      "XMLATTRIBUTES(s.name AS \"name\"), " +
+                  " XMLAGG(XMLELEMENT(NAME \"description\", s.description))) " +
+                  " FROM sys_classes s " +
+                  " GROUP BY 1, name " +
+                  " having s.name = 'Контакт'";
+		
+		for (String sql: sqlQuery){
+		     Assert.assertTrue(Main.main(sql, true));
+		}
 	}
 }
