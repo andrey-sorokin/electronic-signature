6758bf21de7f29eefbda700cc38d257e80bd2f36
Andrey.Sorokin <Andrey.Sorokin@R-Style.com>
2013-05-15 20:02:18 +0400

    removed unused any longer files related to oracle query correction

--- sql2xml/src/main/java/ru/rstyle/so/transform/Oracle.java
+++
@@ -1,8 +0,0 @@
-package ru.rstyle.so.transform;
-
-public class Oracle {
-	public static String getAsClob(String input) {
-		return input.replaceAll("(?i)(XMLELEMENT.*?)\\sFROM", "TO_CLOB($1) FROM");
-	}
-
-}
